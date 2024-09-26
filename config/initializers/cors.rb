21bcf60e752900d57dadeefe4e157f233eff23c0
merrelltd72 <tyron.merrell@gmail.com>
2024-09-25 23:26:12 -0500

    Implemented httponlycookie authentication.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:5173"
-    resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    resource "*", 
+    headers: :any,
+     credentials: true,
+    methods: [:get, :post, :patch, :put, :delete]
   end
 end
