cde7b61b72696d95a4fe29ce01355a4a05dcd906
Tyron Merrell <tyron.merrell@gmail.com>
2025-06-28 10:52:28 -0500

    Updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,61 +10,61 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 20_250_414_024_545) do # rubocop:disable Metrics/BlockLength
+ActiveRecord::Schema[8.0].define(version: 2025_06_14_173604) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'pg_catalog.plpgsql'
+  enable_extension "pg_catalog.plpgsql"
 
-  create_table 'accounts', force: :cascade do |t|
-    t.string 'user_id'
-    t.integer 'category_id'
-    t.string 'web_app_name'
-    t.string 'url'
-    t.string 'username'
-    t.string 'password'
-    t.text 'notes'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "accounts", force: :cascade do |t|
+    t.string "user_id"
+    t.integer "category_id"
+    t.string "web_app_name"
+    t.string "url"
+    t.string "username"
+    t.string "password"
+    t.text "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'categories', force: :cascade do |t|
-    t.string 'category_type'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "categories", force: :cascade do |t|
+    t.string "category_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'password_reminders', force: :cascade do |t|
-    t.bigint 'account_id', null: false
-    t.bigint 'user_id', null: false
-    t.datetime 'reminder_date'
-    t.boolean 'notification_sent'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['account_id'], name: 'index_password_reminders_on_account_id'
-    t.index ['user_id'], name: 'index_password_reminders_on_user_id'
+  create_table "password_reminders", force: :cascade do |t|
+    t.bigint "account_id", null: false
+    t.bigint "user_id", null: false
+    t.date "reminder_date"
+    t.boolean "notification_sent", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_password_reminders_on_account_id"
+    t.index ["user_id"], name: "index_password_reminders_on_user_id"
   end
 
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_id'], name: 'index_tasks_on_user_id'
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'username'
-    t.string 'email'
-    t.string 'password_digest'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'provider'
-    t.string 'uid'
-    t.string 'token'
-    t.index %w[provider uid], name: 'index_users_on_provider_and_uid', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
-  add_foreign_key 'password_reminders', 'accounts'
-  add_foreign_key 'password_reminders', 'users'
-  add_foreign_key 'tasks', 'users'
+  add_foreign_key "password_reminders", "accounts"
+  add_foreign_key "password_reminders", "users"
+  add_foreign_key "tasks", "users"
 end
