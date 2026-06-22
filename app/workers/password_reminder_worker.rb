8e4290f5d94ffe52e0f3ca035c5b9bc7bdb079df
Tyron Merrell <171091266+merrelltd72@users.noreply.github.com>
2026-06-21 22:01:45 -0500

    Activity Events created. (#90)

--- app/workers/password_reminder_worker.rb
+++ app/workers/password_reminder_worker.rb
@@ -8,6 +8,20 @@ class PasswordReminderWorker
     PasswordReminder.due_reminders.find_each do |reminder|
       PasswordReminders::Delivery.broadcast(reminder)
       reminder.mark_notified!
+
+      activity_event(reminder)
     end
+  end
+
+  private
+
+  def activity_event(reminder)
+    ActivityEvent.create!(
+      user: reminder.user,
+      event_type: 'reminder_completed',
+      subject_type: 'PasswordReminder',
+      subject_id: reminder.id,
+      metadata: { account_id: reminder.account.id, reminder_date: reminder.reminder_date }
+    )
   end
 end
